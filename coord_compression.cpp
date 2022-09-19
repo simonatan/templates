e555128628823768352f182895e1fbb62a49efbb
Simona Tankova <82508193+simonatan@users.noreply.github.com>
2022-09-20 00:05:10 +0300

    Add files via upload

---
+++ coord_compression.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+void coord_compression(std::vector<int> a, int n) {
+  std::vector<int> b = a;
+  std::sort(b.begin(), b.end());
+  b.resize(std::unique(b.begin(), b.end()) - b.begin());
+  for(int i = 0; i < n; i++) {
+    a[i] = std::lower_bound(b.begin(), b.end(), a[i]) - b.begin();
+  }
+}
